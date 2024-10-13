56a1aeaf82c6249216d82dd1d5ff3988337ba736
Sanchit Asoliya <sanchit30469@gmail.com>
2024-10-13 17:48:55 +0530

    Quartz sync: Oct 13, 2024, 5:48 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -25,7 +25,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "'Bricolage Grotesque', sans-serif", // Add single quotes around multi-word font names
-        body: "'Bricolage Grotesque', sans-serif",
+        body: "Montserrat, sans-serif",
         code: "IBM Plex Mono",
       },
       colors: {
