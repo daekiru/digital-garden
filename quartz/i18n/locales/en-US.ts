44569cfa289707ad466c2b920a6e70f0f0605ead
Sanchit Asoliya <sanchit30469@gmail.com>
2024-10-14 10:50:14 +0530

    Quartz sync: Oct 14, 2024, 10:50 AM

--- quartz/i18n/locales/en-US.ts
+++ quartz/i18n/locales/en-US.ts
@@ -33,7 +33,7 @@ export default {
       title: "Explorer",
     },
     footer: {
-      createdWith: "Built by Sanchit via",
+      createdWith: "Built with ❤️ by Sanchit via",
     },
     graph: {
       title: "Graph View",
