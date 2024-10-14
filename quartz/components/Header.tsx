f496d0c97e3e17924884215defa001a2c4ffbf74
Sanchit Asoliya <sanchit30469@gmail.com>
2024-10-14 18:59:13 +0530

    Quartz sync: Oct 14, 2024, 6:59 PM

--- quartz/components/Header.tsx
+++ quartz/components/Header.tsx
@@ -1,29 +1,22 @@
-import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types";
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 const Header: QuartzComponent = ({ children }: QuartzComponentProps) => {
-  return children.length > 0 ? (
-    <header>
-      {/* Add the icon image here */}
-      <img src="/static/icon.png" alt="Icon" style={{ height: "50px", marginRight: "1rem" }} />
-      {children}
-    </header>
-  ) : null;
-};
+  return children.length > 0 ? <header>{children}</header> : null
+}
 
 Header.css = `
-  header {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    margin: 2rem 0;
-    gap: 1.5rem;
-  }
+header {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 2rem 0;
+  gap: 1.5rem;
+}
 
-  header h1 {
-    margin: 0;
-    flex: auto;
-  }
-`;
+header h1 {
+  margin: 0;
+  flex: auto;
+}
+`
 
-export default (() => Header) satisfies QuartzComponentConstructor;
-
+export default (() => Header) satisfies QuartzComponentConstructor
